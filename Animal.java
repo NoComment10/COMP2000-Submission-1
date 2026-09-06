aba5305732dcf003dfa09cf0bc9edea6011133bd
Riya Chauhan <riya.chauhan1@students.mq.edu.au>
2026-09-06 17:38:30 +1000

    created Animal.java that animals will inherit from

---
+++ Animal.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.List;
+
+public abstract class Animal {
+    Color color;
+    List<Polygon> display;
+    Cell cell;
+    int movesWithoutFood;
+    int movesSinceReproduction;
+
+    public Animal(Cell cell) {
+        this.cell = cell;
+    }
+
+    public void paint(Graphics g) {
+        for (Polygon p : display) {
+            g.setColor(color);
+            g.fillPolygon(p);
+            g.setColor(Color.GRAY);
+            g.drawPolygon(p);
+        }
+    }
+
+    public abstract Animal reproduce(Cell cell);
+}
