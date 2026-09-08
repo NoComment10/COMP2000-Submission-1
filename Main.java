311bc49eb8f3a40d745f366d10c288246faab395
Riya Chauhan <riya.chauhan1@students.mq.edu.au>
2026-09-09 01:15:06 +1000

    added a scanner to ask user how many turns they want to see, added checks for valid input, fixed run() so that it runs the simulation while showing each turn

--- Main.java
+++ Main.java
@@ -1,26 +1,48 @@
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import java.awt.Dimension;
+import java.util.Scanner;
+import javax.swing.Timer;
+
 
 public class Main extends JFrame {
+    Timer timer;
+    Simulation stage = new Simulation();
+
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfTurns;
+
+        System.out.println("Please type how many turns the simulation should last (positive integer answer only)");
+        while(true) {
+            String input = scanner.nextLine();
+
+            try {
+                numberOfTurns = Integer.parseInt(input);
+                
+                if(numberOfTurns > 0) {
+                    break;
+                }
+            }
+            catch(NumberFormatException e) {
+                //not valid
+            }
+            System.out.println("Invalid input. Please type a positive integer to select how many turns the simulation should last");
+        }
         Main window = new Main();
-        window.run();
+        window.run(numberOfTurns);
+        scanner.close();
     }
 
 
     class Canvas extends JPanel {
 
-        Simulation stage = new Simulation();
         //sets the size of the canvas to 700x700 pixels
         public Canvas() {
             setPreferredSize(new Dimension(700, 700));
         }
 
-        //draws a black rectangle on the canvas
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -36,9 +58,18 @@ public class Main extends JFrame {
         this.setVisible(true);
     }
 
-    public void run() {
-        while (true) {
-            this.repaint();
-        }
+    public void run(int numberOfTurns) {
+        int[] turns ={0};
+
+        timer = new Timer (750, e -> {
+            stage.runTurn();
+            repaint();
+            turns[0]++;
+
+            if(turns[0] >= numberOfTurns) {
+                timer.stop();
+            }
+        });
+        timer.start();
     }
 }
